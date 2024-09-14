a7962a188d07574725df399d7d4e5b3583a34b1c
Masahiro OHTA <masahiro.ohta0211@gmail.com>
2024-09-14 22:56:03 +0100

    Minor code style change and clean up

--- ch_01_simple_mc/simple_mc_main_1.cpp
+++ ch_01_simple_mc/simple_mc_main_1.cpp
@@ -7,72 +7,72 @@
 #include <iostream>
 #include <cmath>
 
-double simpleMonteCarlo1(double expiry,
-						 double strike,
-						 double spot,
-						 double vol,
-						 double rate,
-						 unsigned long numberOfPaths)
+namespace
 {
-
-	double variance = vol*vol*expiry;
-	double rootVariance = std::sqrt(variance);
-	double itoCorrection = -0.5*variance;
-
-	double movedSpot = spot*exp(rate * expiry +itoCorrection);
-	double thisSpot;
-	double runningSum=0;
-
-	for (unsigned long i=0; i < numberOfPaths; i++)
+	double simpleMonteCarlo1(double timeToExpiry,
+		double strike,
+		double spot,
+		double vol,
+		double rate,
+		unsigned long numberOfPaths)
 	{
-		double thisGaussian = getOneGaussianByBoxMuller();
-		thisSpot = movedSpot * std::exp( rootVariance*thisGaussian);
-		double thisPayoff = thisSpot - strike;
-    	thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-		runningSum += thisPayoff;
+
+		double variance = vol * vol * timeToExpiry;
+		double rootVariance = std::sqrt(variance);
+		double itoCorrection = -0.5 * variance;
+
+		double movedSpot = spot * std::exp(rate * timeToExpiry + itoCorrection);
+		double thisSpot;
+		double runningSum = 0;
+
+		for (unsigned long i = 0; i < numberOfPaths; i++)
+		{
+			double thisGaussian = getOneGaussianByBoxMuller();
+			thisSpot = movedSpot * std::exp(rootVariance * thisGaussian);
+			double thisPayoff = thisSpot - strike;
+			thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
+			runningSum += thisPayoff;
+		}
+
+		double mean = runningSum / numberOfPaths;
+		mean *= std::exp(-rate * timeToExpiry);
+		return mean;
 	}
 
-	double mean = runningSum / numberOfPaths;
-	mean *= std::exp(-rate * expiry);
-	return mean;
-}
+	template <typename T>
+	void inputValue(T& value, const std::string& msg)
+	{
+		std::cout << msg << ": ";
+		std::cin >> value;
+		// std::cout << "\n";
+		return;
+	}
+} // namespace
 
 int main()
 {
-
-	double expiry;
+	double timeToExpiry;
 	double strike; 
 	double spot; 
 	double vol; 
 	double rate;
 	unsigned long numberOfPaths;
 
-	std::cout << "\nEnter expiry\n";
-	std::cin >> expiry;
+	inputValue(timeToExpiry, "Enter timeToExpiry");
+	inputValue(strike, "Enter strike");
+	inputValue(spot, "Enter spot");
+	inputValue(vol, "Enter vol");
+	inputValue(rate, "Enter rate");
+	inputValue(numberOfPaths, "Enter number of paths");
 
-	std::cout << "\nEnter strike\n";
-	std::cin >> strike;
-
-	std::cout << "\nEnter spot\n";
-	std::cin >> spot;
-
-	std::cout << "\nEnter vol\n";
-	std::cin >> vol;
-
-	std::cout << "\nr\n";
-	std::cin >> rate;
-
-	std::cout << "\nNumber of paths\n";
-	std::cin >> numberOfPaths;
-
-	double result = simpleMonteCarlo1(expiry,
+	double result = simpleMonteCarlo1(timeToExpiry,
                                       strike, 
 							          spot, 
 							          vol, 
 							          rate, 
 						              numberOfPaths);
 
-	std::cout <<"the price is " << result << "\n";
+	std::cout <<"the price is " << result << std::endl;
 
     double tmp;
     std::cin >> tmp;
