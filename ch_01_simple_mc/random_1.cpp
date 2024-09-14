51d5f82163400eb8c8d3bed3e9f72ba12b1199b6
Masahiro OHTA <masahiro.ohta0211@gmail.com>
2024-09-14 23:09:14 +0100

    Replace tab with space

--- ch_01_simple_mc/random_1.cpp
+++ ch_01_simple_mc/random_1.cpp
@@ -9,37 +9,37 @@
 
 double getOneGaussianBySummation()
 {
-	double result=0;
+    double result=0;
 
-	for (unsigned long j=0; j < 12; j++)
-		result += std::rand()/static_cast<double>(RAND_MAX);
+    for (unsigned long j=0; j < 12; j++)
+        result += std::rand()/static_cast<double>(RAND_MAX);
 
-	result -= 6.0;
+    result -= 6.0;
 
-	return result;
+    return result;
 
 }
 
 
 double getOneGaussianByBoxMuller()
 {
-	double result;
+    double result;
 
-	double x;
-	double y;
+    double x;
+    double y;
 
-	double sizeSquared;
-	do
-	{
-		x = 2.0 * std::rand()/static_cast<double>(RAND_MAX)-1;
-		y = 2.0 * std::rand()/static_cast<double>(RAND_MAX)-1;
-		sizeSquared = x*x + y*y;
-	}
-	while
-		( sizeSquared >= 1.0);
+    double sizeSquared;
+    do
+    {
+        x = 2.0 * std::rand()/static_cast<double>(RAND_MAX)-1;
+        y = 2.0 * std::rand()/static_cast<double>(RAND_MAX)-1;
+        sizeSquared = x*x + y*y;
+    }
+    while
+        ( sizeSquared >= 1.0);
 
-	result = x * std::sqrt(-2.0 * std::log(sizeSquared) / sizeSquared);
+    result = x * std::sqrt(-2.0 * std::log(sizeSquared) / sizeSquared);
 
-	return result;
+    return result;
 
 }
--- ch_01_simple_mc/simple_mc_main_1.cpp
+++ ch_01_simple_mc/simple_mc_main_1.cpp
@@ -9,73 +9,73 @@
 
 namespace
 {
-	double simpleMonteCarlo1(double timeToExpiry,
-		double strike,
-		double spot,
-		double vol,
-		double rate,
-		unsigned long numberOfPaths)
-	{
+    double simpleMonteCarlo1(double timeToExpiry,
+        double strike,
+        double spot,
+        double vol,
+        double rate,
+        unsigned long numberOfPaths)
+    {
 
-		double variance = vol * vol * timeToExpiry;
-		double rootVariance = std::sqrt(variance);
-		double itoCorrection = -0.5 * variance;
+        double variance = vol * vol * timeToExpiry;
+        double rootVariance = std::sqrt(variance);
+        double itoCorrection = -0.5 * variance;
 
-		double movedSpot = spot * std::exp(rate * timeToExpiry + itoCorrection);
-		double thisSpot;
-		double runningSum = 0;
+        double movedSpot = spot * std::exp(rate * timeToExpiry + itoCorrection);
+        double thisSpot;
+        double runningSum = 0;
 
-		for (unsigned long i = 0; i < numberOfPaths; i++)
-		{
-			double thisGaussian = getOneGaussianByBoxMuller();
-			thisSpot = movedSpot * std::exp(rootVariance * thisGaussian);
-			double thisPayoff = thisSpot - strike;
-			thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-			runningSum += thisPayoff;
-		}
+        for (unsigned long i = 0; i < numberOfPaths; i++)
+        {
+            double thisGaussian = getOneGaussianByBoxMuller();
+            thisSpot = movedSpot * std::exp(rootVariance * thisGaussian);
+            double thisPayoff = thisSpot - strike;
+            thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
+            runningSum += thisPayoff;
+        }
 
-		double mean = runningSum / numberOfPaths;
-		mean *= std::exp(-rate * timeToExpiry);
-		return mean;
-	}
+        double mean = runningSum / numberOfPaths;
+        mean *= std::exp(-rate * timeToExpiry);
+        return mean;
+    }
 
-	template <typename T>
-	void inputValue(T& value, const std::string& msg)
-	{
-		std::cout << msg << ": ";
-		std::cin >> value;
-		// std::cout << "\n";
-		return;
-	}
+    template <typename T>
+    void inputValue(T& value, const std::string& msg)
+    {
+        std::cout << msg << ": ";
+        std::cin >> value;
+        // std::cout << "\n";
+        return;
+    }
 } // namespace
 
 int main()
 {
-	double timeToExpiry;
-	double strike; 
-	double spot; 
-	double vol; 
-	double rate;
-	unsigned long numberOfPaths;
+    double timeToExpiry;
+    double strike; 
+    double spot; 
+    double vol; 
+    double rate;
+    unsigned long numberOfPaths;
 
-	inputValue(timeToExpiry, "Enter timeToExpiry");
-	inputValue(strike, "Enter strike");
-	inputValue(spot, "Enter spot");
-	inputValue(vol, "Enter vol");
-	inputValue(rate, "Enter rate");
-	inputValue(numberOfPaths, "Enter number of paths");
+    inputValue(timeToExpiry, "Enter timeToExpiry");
+    inputValue(strike, "Enter strike");
+    inputValue(spot, "Enter spot");
+    inputValue(vol, "Enter vol");
+    inputValue(rate, "Enter rate");
+    inputValue(numberOfPaths, "Enter number of paths");
 
-	double result = simpleMonteCarlo1(timeToExpiry,
+    double result = simpleMonteCarlo1(timeToExpiry,
                                       strike, 
-							          spot, 
-							          vol, 
-							          rate, 
-						              numberOfPaths);
+                                      spot, 
+                                      vol, 
+                                      rate, 
+                                      numberOfPaths);
 
-	std::cout <<"the price is " << result << std::endl;
+    std::cout <<"the price is " << result << std::endl;
 
     double tmp;
     std::cin >> tmp;
 
-	return 0;
+    return 0;
 }
