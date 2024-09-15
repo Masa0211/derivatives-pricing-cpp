30160d4277d3d73e4768be6f4a1a96707409a6bd
Masahiro OHTA <masahiro.ohta0211@gmail.com>
2024-09-15 16:29:36 +0100

    Change I/O in the main

--- ch_03_inheritance/src/simple_mc_main_4.cpp
+++ ch_03_inheritance/src/simple_mc_main_4.cpp
@@ -4,64 +4,62 @@
 */
 
 #include "simple_mc_2.h"
-#include<iostream>
-using namespace std;
+#include <iostream>
 
 int main()
 {
+    // Don't want to input every single value when I'm debugging
+    double timeToExpiry = 0.5;
+    double strike = 100.0;
+    double spot = 95.0;
+    double vol = 0.2;
+    double rate = 0.05;
+    unsigned long numberOfPaths = 500000;
+    char cp = 'c';
 
-	double Expiry;
-	double Strike; 
-	double Spot; 
-	double Vol; 
-	double r; 
-	unsigned long NumberOfPaths;
+    std::cout << "Default values..." << std::endl;
+    std::cout << "timeToExpiry  : " << timeToExpiry << std::endl;
+    std::cout << "strike        : " << strike << std::endl;
+    std::cout << "spot          : " << spot << std::endl;
+    std::cout << "vol           : " << vol << std::endl;
+    std::cout << "rate          : " << rate << std::endl;
+    std::cout << "numberOfPaths : " << numberOfPaths << std::endl;
+    std::cout << "call/put [c/p]: " << cp << std::endl;
 
-	cout << "\nEnter expiry\n";
-	cin >> Expiry;
+    std::cout << "Do you want to input values? [Y/N]: ";
+    char yn; std::cin >> yn;
+    if (std::tolower(yn) == 'y')
+    {
+        std::cout << "Enter values..." << std::endl;
+        std::cout << "timeToExpiry  : "; std::cin >> timeToExpiry;
+        std::cout << "strike        : "; std::cin >> strike;
+        std::cout << "spot          : "; std::cin >> spot;
+        std::cout << "vol           : "; std::cin >> vol;
+        std::cout << "rate          : "; std::cin >> rate;
+        std::cout << "numberOfPaths : "; std::cin >> numberOfPaths;
+        std::cout << "call/put [c/p]: "; std::cin >> cp;
+    }
 
-	cout << "\nEnter strike\n";
-	cin >> Strike;
-
-	cout << "\nEnter spot\n";
-	cin >> Spot;
-
-	cout << "\nEnter vol\n";
-	cin >> Vol;
-
-	cout << "\nr\n";
-	cin >> r;
-
-	cout << "\nNumber of paths\n";
-	cin >> NumberOfPaths;
-
-
-    unsigned long optionType;
-
-    cout << "\nenter 0 for call, otherwise put ";
-    cin >> optionType;
-
-
-    PayOff* thePayOffPtr; 
-
-    if (optionType== 0)
-        thePayOffPtr = new PayOffCall(Strike);
+    PayOff* thePayOffPtr;
+    if (std::tolower(cp) == 'c')
+        thePayOffPtr = new PayOffCall(strike);
     else
-        thePayOffPtr = new PayOffPut(Strike);
+        thePayOffPtr = new PayOffPut(strike);
 
-	double result = simpleMonteCarlo2(*thePayOffPtr,
-                                      Expiry,
-                                      Spot, 
-                                      Vol,
-                                      r,
-                                      NumberOfPaths);
-	
-	cout <<"\nthe price is " << result << "\n";
+    double result = simpleMonteCarlo2(
+        *thePayOffPtr,
+        timeToExpiry,
+        spot,
+        vol,
+        rate,
+        numberOfPaths);
+
+    std::cout << "price: " << result << std::endl;
     double tmp;
-    cin >> tmp;
+    std::cin >> tmp;
 
     delete thePayOffPtr;
 
-	return 0;
+    return 0;
 
 }
